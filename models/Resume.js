6b2a40ec73da626dd5d0ada18212489878327485
Carl Kakisis <Messinias1@gmail.com>
2019-06-20 13:29:24 -0400

    created resume schema.. first page

---
+++ models/Resume.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ResumeSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipCode: {
+        type: String,
+        required: true
+    },
+    cellPhone: {
+        type: String,
+        required: true
+    },
+    personalEmail: {
+        type: String,
+        required: true
+    },
+    githubURL: {
+        type: String,
+        required: true
+    },
+    twitterURL: {
+        type: String,
+        required: true
+    },
+    personalWebsiteURL: {
+        type: String
+    },
+    aboutMeStatement: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Resume = mongoose.model('resumes', ResumeSchema)
