1cd53e36cd7910364d9a4de094320d357e1cb77a
m-tartag <mattbrs51@gmail.com>
2019-06-22 01:30:10 -0400

    installed bcrypt, express-validator, config

--- routes/api/register.js
+++ routes/api/register.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route     POST api/register
 // @desc      Test Route
