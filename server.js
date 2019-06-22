c761141c7ac7fa653896788dd717b77a6395b25c
m-tartag <mattbrs51@gmail.com>
2019-06-21 23:54:01 -0400

    update

--- server.js
+++ server.js
@@ -2,30 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+
 const port = process.env.Port || 4000;
 const keys = require('./config/keys');
-
 
 const app = express();
 // Middleware BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoDBUrl, { useNewUrlParser: true }).then(()=>{
-    console.log(`MongoDB Connected`)
-})
+mongoose.connect(keys.mongoDBUrl, { useNewUrlParser: true }).then(() => {
+  console.log(`MongoDB Connected`);
+});
 
 // Passport middleware
 app.use(passport.initialize());
-//Passport Config
+// Passport Config
 require('./config/passport')(passport);
 
-app.listen(port, err =>{
-    if (err) throw err 
-    console.log(`App currently listening on port ${port}`);
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`App currently listening on port ${port}`);
 });
-
-
