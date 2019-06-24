91c8c553be02d3e1d5464f745a73ba738e15769c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-24 01:02:12 -0400

    updated ResumeForm.js

--- client/src/components/layouts/ResumeForm.js
+++ client/src/components/layouts/ResumeForm.js
@@ -1,42 +1,35 @@
 import React, { Component } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 import classnames from "classnames";
 
+
 class ResumeForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      address: "",
-      city: "",
-      state: "",
-      zipcode: "",
-      cellPhone: "",
-      personalEmail: "",
-      githubURL: "",
-      twitterURL: "",
-      personalWebsiteURL: "",
-      aboutMeStatement: "",
-      errors: {}
-    };
-  }
+  state = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    cellPhone: "",
+    personalEmail: "",
+    githubURL: "",
+    twitterURL: "",
+    personalWebsiteURL: "",
+    aboutMeStatement: "",
+    errors: {}
+  };
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors
-      });
-
-    }
-  }
+  
   onChange = e => {
-    this.setState({ [e.target.id]: e.target.value })
-  }
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
-  onSubmit = () => {
+  newResume = (e) => {
+    e.preventDefault();
     // Data Payload
-    const data = {
+    const newResume = {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       address: this.state.address,
@@ -51,107 +44,178 @@ class ResumeForm extends Component {
       aboutMeStatement: this.state.aboutMeStatement
     };
 
-    
-    axios.post("/api/resume", data)
-    .then(resume => console.log(resume.data))
+    axios.post('/api/resume/resumeForm', newResume)
+    .then(res => {
+      if (res.status !==200){
+        res.json()
+        .then(res => console.log(res))
+      } else {
+        console.log(res.data)
+      }
+    })
+      
+  
   };
+
+
   render() {
     const { errors } = this.state;
     return (
-      <div class="row">
-      <h3 class="align center">Insert Resume Details</h3>
-      <form class="col s6 offset-m3" >
-          <div class="row">
-            <div class="input-field col s4">
-              <input
-                onchange={this.onChange}
-                value={this.state.firstName}
-                error={errors.firstName}
-                placeholder="First Name"
-                id="first_name"
-                type="text"
-                class="validate"
-                className={classnames("", {
-                  isvalid: errors.firstName
-                })}
-              />
+      <div className="container">
+        <h3 className="align center">Insert Resume Details</h3>
+        <div className="row">
+          <form
+            noValidate
+            onSubmit={this.newResume}
+            className="col s8 offset-s2"
+          >
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  onChange={this.onChange}
+                  value={this.state.firstName}
+                  error={errors.firstName}
+                  placeholder="First Name"
+                  id="firstName"
+                  type="text"
+                  className={classnames("", {
+                    isvalid: errors.firstName
+                  })}
+                />
+                <label htmlFor="firstName">First Name</label>
+                <span className="red-text">{errors.firstName}</span>
 
-              <input
-                placeholder="Last Name"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.lastName}
+                  error={errors.lastName}
+                  placeholder="Last Name"
+                  id="lastName"
+                  type="text"
+                  
+                />
 
-              <input
-                placeholder="Address Name"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.address}
+                  error={errors.address}
+                  placeholder="Address Name"
+                  id="address"
+                  type="text"
+                  
+                />
 
-              <input
-                placeholder="City"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.city}
+                  error={errors.city}
+                  placeholder="City"
+                  id="city"
+                  type="text"
+                  
+                />
 
-              <input
-                placeholder="State"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.state}
+                  error={errors.state}
+                  placeholder="State"
+                  id="state"
+                  type="text"
+                  
+                />
 
-              <input
-                placeholder="Zipcode"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
-              <input
-                placeholder="CellPhone"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.zipcode}
+                  error={errors.zipcode}
+                  placeholder="Zipcode"
+                  id="zipcode"
+                  type="text"
+                 
+                />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.cellPhone}
+                  error={errors.cellPhone}
+                  placeholder="CellPhone"
+                  id="cellPhone"
+                  type="text"
+                  
+                />
 
-              <input
-                placeholder="Email"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.personalEmail}
+                  error={errors.personalEmail}
+                  placeholder="Email"
+                  id="personalEmail"
+                  type="text"
+                 
+                />
 
-              <input
-                placeholder="Github"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.githubURL}
+                  error={errors.githubURL}
+                  placeholder="Github"
+                  id="githubURL"
+                  type="text"
+                  
+                />
 
-              <input
-                placeholder="Twitter"
-                id="first_name"
-                type="text"
-                class="validate"
-              />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.twitterURL}
+                  error={errors.twitterURL}
+                  placeholder="Twitter"
+                  id="twitterURL"
+                  type="text"
+                  
+                />
+                <input
+                  onChange={this.onChange}
+                  value={this.state.personalWebsiteURL}
+                  error={errors.personalWebsiteURL}
+                  placeholder="Personal Website"
+                  id="personalWebsiteURL"
+                  type="text"
+                  
+                />
 
-              <textarea
-                id="textarea1"
-                class="materialize-textarea"
-                placeholder="About Me"
-              />
-              <button class="btn waves-effect waves-light align right blue" type="submit" name="action">Submit
-            <i class="material-icons right">send</i>
-          </button>
+                <textarea
+                  onChange={this.onChange}
+                  value={this.state.aboutMeStatement}
+                  error={errors.aboutMeStatement}
+                  id="aboutMeStatement"
+                  class="materialize-textarea"
+                  placeholder="About Me"
+                />
+              </div>
+              <div className="col s12">
+              <button 
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                type="submit"
+                className="btn btn-large waves-effect waves-light hoverable green accent-3 align right"
+              >
+                Submit
+              </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
 }
+ResumeForm.propTypes = {
+  errors: PropTypes.object.isRequired
+}
+
 
 export default ResumeForm;
