d0e39afa3394bbbddfa842895e8a7046c8689189
m-tartag <mattbrs51@gmail.com>
2019-06-24 00:00:00 -0400

    added github links to About

--- client/src/components/layouts/About.js
+++ client/src/components/layouts/About.js
@@ -4,13 +4,14 @@ const About = () => {
   return (
     <div>
       <div id='About'>
-        <h4>This is a full-stack MERN app for creating, displaying, and searching for resumes.</h4>
-        <h5>The Team:</h5>
-        <ul><li>Victor Montanez</li>
-          <li>Matt Tartaglia</li>
-          <li>Jethro Glaudin</li>
-          <li>Carl Kakisis</li>
-          <li>Oscar Diahn</li>
+        <h4>This is a full-stack MERN app for creating, displaying, and searching for resumes.</h4><br/>
+        <h5 style={{ fontWeight: 'bold'}}>The Team:</h5>
+        <ul id='Team'>
+          <li><a href="https://github.com/jethroglaudin">Jethro Glaudin</a></li>
+          <li><a href="https://github.com/m-tartag">Matt Tartaglia</a></li>
+          <li><a href="https://github.com/VicMontanez">Victor Montanez</a></li>
+          <li><a href="https://github.com/Messinias1">Carl Kakisis</a></li>
+          <li><a href="#">Oscar Diahn</a></li>
         </ul>
       </div>
     </div>
